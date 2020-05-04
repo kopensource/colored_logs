f0f46d99fc19763641c3ad8e42a4d96eb57dc894
Kristóf Kovács <kristof.kovacs@telenav.com>
2020-05-04 23:40:31 +0300

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -17,19 +17,22 @@ from colored_logs.logger import Logger
 
 class LogTestCase(unittest.TestCase):
     def test_logs(self):
-        log = Logger(ID='Test-id-1')
+        try:
+            log = Logger(ID='Test-id-1')
 
-        log.info('This is an info log')
-        log.success('This is a success log')
-        log.warning('This is a warning log')
-        log.error('This is an error log')
-        log.fail('This is a fail log')
-        log.critical('This is a critical log')
-        log.subtle('This is a subtle log')
+            log.info('This is an info log')
+            log.success('This is a success log')
+            log.warning('This is a warning log')
+            log.error('This is an error log')
+            log.fail('This is a fail log')
+            log.critical('This is a critical log')
+            log.subtle('This is a subtle log')
 
-        log.start_process('This will take a while')
-        time.sleep(1)
-        log.stop_process()
+            log.start_process('This will take a while')
+            time.sleep(1)
+            log.stop_process()
+        except Exception as e:
+            self.fail('Failed main test with error: ' + repr(e))
 
 if __name__ == '__main__':
     unittest.main()
