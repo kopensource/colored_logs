6e176c6073482b1985ad5f9048707284bed885ad
Kristóf Kovács <kristof.kovacs@telenav.com>
2020-05-04 19:49:54 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="colored_logs",
+    version="0.2.3",
+    author="Kovacs Kristof-Attila",
+    description="A colored logs package based on 'colored'",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/ColoredLogs",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
