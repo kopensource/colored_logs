2fbb936602e71b6a217ee85a57c10856662f2a7b
Kristóf Kovács <kristof.kovacs@telenav.com>
2020-05-05 23:48:19 +0300

    updated tests

--- demo.py
+++ demo.py
@@ -9,32 +9,32 @@ log = Logger(
 )
 
 log.info('This is an info log')
-time.sleep(0.5)
+time.sleep(0.1)
 
 log.ID='Test-id-2'
 log.info('This is an info log with a new id')
 log.ID='Test-id-1'
-time.sleep(0.5)
+time.sleep(0.1)
 
 log.success('This is a success log')
-time.sleep(0.5)
+time.sleep(0.1)
 log.warning('This is a warning log')
-time.sleep(0.5)
+time.sleep(0.1)
 log.error('This is an error log')
-time.sleep(0.5)
+time.sleep(0.1)
 log.fail('This is a fail log')
-time.sleep(0.5)
+time.sleep(0.1)
 log.critical('This is a critical log')
-time.sleep(0.5)
+time.sleep(0.1)
 log.subtle('This is a subtle log')
 
-time.sleep(1)
+time.sleep(0.1)
 
 log.start_process('This will take a while')
-time.sleep(3.5)
+time.sleep(2.0)
 log.info('This is an info log while also logging the active process')
 
-time.sleep(3.5)
+time.sleep(2.0)
 
 duration_float_seconds = log.stop_process(
     log_type=LogType.Success,
--- test_all.py
+++ test_all.py
@@ -15,22 +15,10 @@ class ColorTestCase(unittest.TestCase):
 import time
 from colored_logs.logger import Logger
 
-class LogTestCase(unittest.TestCase):
-    def test_logs(self):
+class IntegrationTestCase(unittest.TestCase):
+    def test_demo(self):
         try:
-            log = Logger(ID='Test-id-1')
-
-            log.info('This is an info log')
-            log.success('This is a success log')
-            log.warning('This is a warning log')
-            log.error('This is an error log')
-            log.fail('This is a fail log')
-            log.critical('This is a critical log')
-            log.subtle('This is a subtle log')
-
-            log.start_process('This will take a while')
-            time.sleep(1)
-            log.stop_process()
+            import demo
         except Exception as e:
             self.fail('Failed main test with error: ' + repr(e))
 
